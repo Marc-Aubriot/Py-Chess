d3997e9281791ee90251778b5bf5331c1dcbe180
Aubriot Marc <marc.aubriot@outlook.fr>
2024-03-04 13:12:42 +0100

    spawn board & pieces

--- components/Board.py
+++ components/Board.py
@@ -2,10 +2,10 @@ import pygame
 
 class Board:
 
-    def __init__(self) -> None:
+    def __init__(self, board_width, board_height) -> None:
         self.id = "board_one"   # str: uuid?
         self.tiles = "hashmap"  # hashmap: Maptile_name "A1" : content "empty" "white_rook" "piece_id"
-        self.img = pygame.image.load("./assets/chess_board.png")
+        self.img = pygame.transform.scale(pygame.image.load("./assets/chess_board_1.png"), (board_width,board_height))
 
     # affiche le plateau et les pièces dans une fenêtre de jeu
     def draw(self, display):
--- components/Game.py
+++ components/Game.py
@@ -2,13 +2,17 @@ import pygame
 from pygame import  *
 from components.Board import Board
 from components.Player import Player
+from components.Piece import Piece
 
 class Game:
 
     def __init__(self) -> None:
         # game var
         self.id = "game_one"                        # str: uuid?
-        self.board = Board()                        # object
+        self.width = 800
+        self.height = 800
+        self.tile_size = self.width/8
+        self.board = Board(self.width, self.height)                # object
         self.pieces = self.populate_board()         # array[object*]: contenant les 32 pièces
         #self.players = [Player(0), Player(1)]       # array[object*]: contenant les 2 joueurs
         self.turn_count = 0                         # int: le nombre de tour
@@ -24,7 +28,53 @@ class Game:
 
     # place les pièces sur le plateau de jeu
     def populate_board(self):
-        pass
+        pieces = [
+            # white pawns
+            Piece("white_pawn_1", 0, 0, "A2", self.tile_size),
+            Piece("white_pawn_2", 0, 0, "B2", self.tile_size),
+            Piece("white_pawn_3", 0, 0, "C2", self.tile_size),
+            Piece("white_pawn_4", 0, 0, "D2", self.tile_size),
+            Piece("white_pawn_5", 0, 0, "E2", self.tile_size),
+            Piece("white_pawn_6", 0, 0, "F2", self.tile_size),
+            Piece("white_pawn_7", 0, 0, "G2", self.tile_size),
+            Piece("white_pawn_8", 0, 0, "H2", self.tile_size),
+            # black pawns
+            Piece("black_pawn_1", 0, 1, "A7", self.tile_size),
+            Piece("black_pawn_2", 0, 1, "B7", self.tile_size),
+            Piece("black_pawn_3", 0, 1, "C7", self.tile_size),
+            Piece("black_pawn_4", 0, 1, "D7", self.tile_size),
+            Piece("black_pawn_5", 0, 1, "E7", self.tile_size),
+            Piece("black_pawn_6", 0, 1, "F7", self.tile_size),
+            Piece("black_pawn_7", 0, 1, "G7", self.tile_size),
+            Piece("black_pawn_8", 0, 1, "H7", self.tile_size),
+            # white knight
+            Piece("white_knight_1", 1, 0, "B1", self.tile_size),
+            Piece("white_knight_2", 1, 0, "G1", self.tile_size),
+            # black knight
+            Piece("black_knight_1", 1, 1, "B8", self.tile_size),
+            Piece("black_knight_2", 1, 1, "G8", self.tile_size),
+            # white bishop
+            Piece("white_bishop_1", 2, 0, "C1", self.tile_size),
+            Piece("white_bishop_2", 2, 0, "F1", self.tile_size),
+            # black bishop
+            Piece("black_bishop_1", 2, 1, "C8", self.tile_size),
+            Piece("black_bishop_2", 2, 1, "F8", self.tile_size),
+            # white rook
+            Piece("white_rook_1", 3, 0, "A1", self.tile_size),
+            Piece("white_rook_1", 3, 0, "H1", self.tile_size),
+            # black rook
+            Piece("black_rook_1", 3, 1, "A8", self.tile_size),
+            Piece("black_rook_2", 3, 1, "H8", self.tile_size),
+            # white queen
+            Piece("white_queen_1", 4, 0, "D1", self.tile_size),
+            # black queen
+            Piece("black_queen_1", 4, 1, "D8", self.tile_size),
+            # white king
+            Piece("white_king_1", 5, 0, "E1", self.tile_size),
+            # black king
+            Piece("black_king_1", 5, 1, "E8", self.tile_size),
+        ]
+        return pieces
 
     # termine la partie
     def game_end(self):
@@ -52,5 +102,6 @@ class Game:
         self.clock.tick(15)                                             # wait until next frame
 
         self.board.draw(self.display)
-        #for piece in self.pieces:
-        #    piece.draw()
+        for piece in self.pieces:
+            piece.draw(self.display)
+            
